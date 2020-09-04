8bf286ef0f298b83d10bf36c943ec4e3e61b3072
Vitor Morelli <vitao.miacri@gmail.com>
2020-09-04 16:02:37 -0300

    [feat] Add custom hooks

---
+++ src/hooks/Auth.tsx
@@ -0,0 +1,69 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+interface AuthContextData {
+  user: object;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [authData, setAuthData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@salesManager:token');
+    const user = localStorage.getItem('@salesManager:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@salesManager:token', token);
+    localStorage.setItem('@salesManager:user', JSON.stringify(user));
+
+    setAuthData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@salesManager:token');
+    localStorage.removeItem('@salesManager:user');
+
+    setAuthData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: authData.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
