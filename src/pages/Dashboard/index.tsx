02488dffbb9abe4bf4ad2c3d253c3dd0f9a1d56a
Vitor Morelli <vitao.miacri@gmail.com>
2021-01-18 10:20:44 -0300

    feat: Add dashboard page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from '../../components/Menu';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Menu />
+    </Container>
+  );
+};
+
+export default Dashboard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,14 +5,14 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 // import SignUp from '../pages/SignUp';
-// import Dashboard from '../pages/Dashboard';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
-    {/* <Route path="/signup" exact component={SignUp} />
+    {/* <Route path="/signup" exact component={SignUp} /> */}
 
-    <Route path="/dashboard" component={Dashboard} isPrivate /> */}
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
