6ff525525bcf8ecfb92a2a0699dad357a3245f8e
Vitor Morelli <vitao.miacri@gmail.com>
2021-01-19 09:55:44 -0300

    feat: Add new global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+import Button from '../components/Button';
 
 export default createGlobalStyle`
     * {
@@ -24,5 +25,61 @@ export default createGlobalStyle`
 
   button {
     cursor: pointer;
+  }
+`;
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: flex-start;
+`;
+
+export const Content = styled.div`
+  margin: 0 2rem;
+  width: 100%;
+`;
+
+export const ContainerFormButtons = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 2rem !important;
+`;
+
+export const SaveButton = styled(Button)`
+  max-width: 160px;
+  font-size: 1.5vw;
+  text-transform: none;
+  letter-spacing: 0;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 0;
+
+  svg {
+    margin-right: 0.5rem;
+  }
+`;
+
+export const CancelButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+  max-width: 170px;
+  border-radius: 8px;
+  border: solid 1px #7a7a7a;
+  width: 100%;
+  padding: 0 16px;
+  color: #7a7a7a;
+  font-weight: bold;
+  font-size: 1.5vw;
+  height: 50px;
+  margin-right: 0.5rem;
+
+  svg {
+    margin-right: 0.5rem;
   }
 `;
