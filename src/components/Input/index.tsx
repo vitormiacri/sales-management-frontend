683a99726225a2c52856fa230c5b5f4e115553cf
Vitor Morelli <vitao.miacri@gmail.com>
2021-01-20 16:28:49 -0300

    fix: Change Icon prop to optional

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -13,7 +13,7 @@ import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon: React.ComponentType<IconBaseProps>;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
