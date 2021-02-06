b95d3a45e1d7a3da88ee420705bda9c8d0936759
Vitor Morelli <vitao.miacri@gmail.com>
2021-02-06 17:39:15 -0300

    feat: Add dashboard top

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
+import api from '../../services/api';
+import { Container, Content } from '../../styles/global';
+import Format from '../../utils/format';
 
-import { Container } from './styles';
+import { SumaryTop, Value, Card, Description } from './styles';
 
+interface SumaryValuesData {
+  todayValue: number;
+  weekValue: number;
+  monthValue: number;
+  monthAmount: number;
+}
 const Dashboard: React.FC = () => {
+  const [sumaryValues, setSumaryValues] = useState<SumaryValuesData>({
+    todayValue: 0,
+    weekValue: 0,
+    monthValue: 0,
+    monthAmount: 0,
+  });
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function loadSumary(): Promise<void> {
+      const { data } = await api.get<SumaryValuesData>(
+        'dashboard/sumaryOrdersValue',
+      );
+      setSumaryValues(data);
+    }
+    loadSumary();
+  }, []);
+
   return (
     <Container>
       <Menu />
+      <Content>
+        <SumaryTop>
+          <Card>
+            <Description>Vendas de hoje</Description>
+            <Value>{Format.currencyBRL(sumaryValues.todayValue)}</Value>
+          </Card>
+          <Card>
+            <Description>Vendas na semana</Description>
+            <Value>{Format.currencyBRL(sumaryValues.weekValue)}</Value>
+          </Card>
+          <Card>
+            <Description>Vendas no mês</Description>
+            <Value>{Format.currencyBRL(sumaryValues.monthValue)}</Value>
+          </Card>
+          <Card>
+            <Description>Total de pedidos (mês)</Description>
+            <Value>{sumaryValues.monthAmount}</Value>
+          </Card>
+        </SumaryTop>
+      </Content>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,38 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  height: 100vh;
+export const SumaryTop = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 1.5rem;
+`;
+
+export const Card = styled.div`
+  width: 22%;
+  padding: 0;
+  border-radius: 20px;
+  border: 1px solid #dadada;
+  box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.05);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1.5rem 0;
+`;
+
+export const Value = styled.p`
+  font-size: 2vw;
+  color: #26d723;
+  font-weight: 700;
+  line-height: 1.5;
+  letter-spacing: 1px;
+  margin-top: 0.5rem;
+`;
+
+export const Description = styled.p`
+  font-size: 0.8vw;
+  color: #5c5454;
+  letter-spacing: 0.5px;
+  font-weight: 700;
 `;
